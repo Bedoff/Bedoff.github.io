de56de5016f64a1398deacf607ad59daf65c0cdd
Bedoff <74427832+Bedoff@users.noreply.github.com>
2022-01-31 10:44:26 +0300

    Update polyfills.js

--- js/polyfills.js
+++ js/polyfills.js
@@ -1,6 +1,4 @@
-/**
- * Add startsWith support for IE
- */
+
 if (!String.prototype.startsWith) {
     String.prototype.startsWith = function (searchString, position) {
         position = position || 0;
